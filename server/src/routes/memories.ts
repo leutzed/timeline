fa3aebc4836bdaa75f7ba435facab72c184f6c36
daniel <leutzed@gmail.com>
2023-05-21 23:25:09 -0300

    App finishedgit status

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -24,6 +24,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
         id: memory.id,
         coverUrl: memory.coverUrl,
         excerpt: memory.content.substring(0, 60).concat('...'),
+        createdAt: memory.createdAt,
       }
     })
   })
--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -1,6 +1,6 @@
 export function EmptyMemories() {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[360px] text-center leading-relaxed">
         You haven&apos;t recorded any memories yet, start{' '}
         <a href="" className="underline transition-colors hover:text-gray-50">
